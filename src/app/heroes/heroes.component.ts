cfcf5c968d55fc9f46f3712a7f6e3a5c7188ac34
Kevin Vaysse <vayssek@gmail.com>
2018-05-10 22:34:15 +0200

    [UPDATE] Modification of the heroes component html to add a click event
    The event is on the list which display all the heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import {Component, OnInit} from '@angular/core';
+import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -9,12 +9,13 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero: Hero;
 
-  constructor() { }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  constructor() {}
 
   ngOnInit() {
   }
