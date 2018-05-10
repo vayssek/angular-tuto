90b908139ef72cd6ef042ea60cd92f8c7d587d3f
Kevin Vaysse <vayssek@gmail.com>
2018-05-10 16:34:09 +0200

    [ADD] Adding new component the project for Heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HereosComponent } from './hereos/hereos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HereosComponent
   ],
   imports: [
     BrowserModule
