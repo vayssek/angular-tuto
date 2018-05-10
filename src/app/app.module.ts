04023086111e8535702795379246ed70eeb19dd5
Kevin Vaysse <vayssek@gmail.com>
2018-05-10 16:53:32 +0200

    [ADD] ADD new class Hero and modify of the hereos.component.ts
    Addition of the use of the class in the dedicated component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HereosComponent } from './hereos/hereos.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HereosComponent
+    HeroesComponent
   ],
   imports: [
     BrowserModule
